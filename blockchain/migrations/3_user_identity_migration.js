88595d555f8df18a47de9a455e8be243b1269899
XiaohanLi <70464404+Xiaohan-Li@users.noreply.github.com>
2022-04-11 14:09:20 +0800

    Add files via upload

--- blockchain/migrations/3_user_identity_migration.js
+++ blockchain/migrations/3_user_identity_migration.js
@@ -1,13 +1,18 @@
 const UserIdentity = artifacts.require("UserIdentity");
 const BankLoan = artifacts.require("BankLoan");
 const InsurancePolicy = artifacts.require("InsurancePolicy");
+const CarRental = artifacts.require("CarRental");
+const MicroToken = artifacts.require("MicroToken");
 
 module.exports = async function(deployer, network, accounts) {
   await deployer.deploy(UserIdentity);
   const userIdentityInstance = await UserIdentity.deployed();
+  await deployer.deploy(MicroToken);
+  const microTokenInstance = await MicroToken.deployed();
 
   console.log(userIdentityInstance.address);
 
   await deployer.deploy(BankLoan, userIdentityInstance.address);
   await deployer.deploy(InsurancePolicy, userIdentityInstance.address, {from: accounts[3]});
+  await deployer.deploy(CarRental, userIdentityInstance.address, microTokenInstance.address, {from: accounts[4]});
 };
